975a1fd18ffdc95fb2c832c9cfb47883f8632d16
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-01-13 23:12:28 +0100

    add exceptionhandler returning error json

--- src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package com.luv2code.demo.rest;
 import com.luv2code.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,21 +41,30 @@ public class StudentRestController {
     @GetMapping("/students/{studentId}") // return student at index studentId, studentId is a path variable
     public Student getStudent(@PathVariable int studentId) {
 
-        if (studentId >= theStudents.size() || studentId < 0) {
+        // check the studentId against the list size
+
+        if ((studentId >= theStudents.size()) || (studentId < 0)) {
             throw new StudentNotFoundException("Student id not found - " + studentId);
         }
 
         return theStudents.get(studentId);
     }
 
+    // Add an exception handler using @ExceptionHandler
+
     @ExceptionHandler
-    public StudentErrorResponse handleStudentNotFoundException(StudentNotFoundException exc) {
+    public ResponseEntity<StudentErrorResponse> handleStudentNotFoundException(StudentNotFoundException exc) {
 
-        StudentErrorResponse studentErrorResponse = new StudentErrorResponse();
-        studentErrorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        studentErrorResponse.setMessage(exc.getMessage());
-        studentErrorResponse.setTimeStamp(System.currentTimeMillis());
+        // create a StudentErrorResponse
 
-        return new StudentErrorResponse();
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
